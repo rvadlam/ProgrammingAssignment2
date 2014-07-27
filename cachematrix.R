13b670180cbc34e838589cd7135575cadbe4d708
Ranga Vadlamudi <vadlamudi@hotmail.com>
2014-07-27 16:15:13 -0500

    programming assignment 2 commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The makeCacheMatrix function creates a special "matrix", 
+## which is really a list containing a function to
+## set the value of the matrix
+## get the value of the matrix
+## set the value of the inverse matrix
+## get the value of the inverse matrix
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(my_matrix = matrix()) {
+        my_matrixinverse <- NULL
+        set <- function(temp_matrix) {
+                my_matrix <<- temp_matrix
+                my_matrixinverse <<- NULL
+        }
+        get <- function() my_matrix
+        setmatrixinverse <- function(matrixinverse) my_matrixinverse <<- matrixinverse
+        getmatrixinverse <- function() my_matrixinverse
+        list(set = set, get = get,
+             setmatrixinverse = setmatrixinverse,
+             getmatrixinverse = getmatrixinverse)
+        
 }
 
 
-## Write a short comment describing this function
-
+## The following function calculates the matrix inverse of the special 
+## "matrix" created with the above function. However, it first checks to see if the 
+## inverse matrix has already been calculated. 
+## If so, it gets the inverse matrix from the cache and skips the computation. 
+## Otherwise, it calculates the inverse matrix of the data using solve function.
+## ginv function can also be used but MASS package needs to be loaded first
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        my_matrixinverse <- x$getmatrixinverse()
+        if (!is.null(my_matrixinverse)) {
+                message("getting cached data")
+                return(my_matrixinverse)
+        }
+        data <- x$get()
+        my_matrixinverse <- solve(data,...)
+        x$setmatrixinverse(my_matrixinverse)
+        return(my_matrixinverse)
 }
